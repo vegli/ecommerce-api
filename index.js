cbc7f951ae19c301426191999ceeacdaa0f83a02
Veljko Gligorijevic <gligorijevicveljko3@gmail.com>
2022-09-21 00:32:35 +0200

    Login, Register rute

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => console.log("Database connection successfull"))
+.catch((err)=> {
+    console.log(err);
+});
+
+app.use(express.json());
+
+// ROUTES - endpointoiv
+app.use("/api/users", userRoute); //    dakle localhost:5000/api/user/usertest(iz user.js! to je ovaj userRoute)
+
+app.use("/api/auth", authRoute);
 
 
-
-app.listen(5000, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend sever is running!")
 })
